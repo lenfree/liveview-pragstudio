65e377514d81a75f7f830f839b354df4bee4a031
Lenfree Yeung <lenfree.yeung@gmail.com>
2020-07-04 14:50:21 +1000

    add refresh select drop down value

--- lib/live_view_studio_web/live/sales_dashboard_live.ex
+++ lib/live_view_studio_web/live/sales_dashboard_live.ex
@@ -4,10 +4,11 @@ defmodule LiveViewStudioWeb.SalesDashboardLive do
   alias LiveViewStudio.Sales
 
   def mount(_params, _session, socket) do
+    socket =
+      assign_stats(socket)
+      |> assign(refresh: 1)
 
-    if connected?(socket), do: :timer.send_interval(1000, self(), :tick)
-
-    socket = assign_stats(socket)
+    if connected?(socket), do: schedule_refresh(socket)
 
     {:ok, socket}
   end
@@ -43,12 +44,27 @@ defmodule LiveViewStudioWeb.SalesDashboardLive do
         </div>
       </div>
 
-      <button phx-click="refresh">
-        <img src="images/refresh.svg">
-        Refresh
-      </button>
+      <div class="controls">
+        <form phx-change="select-refresh">
+          <label for="refresh">
+            Refresh every:
+          </label>
+          <select name="refresh">
+            <%= options_for_select(refresh_options(), @refresh) %>
+          </select>
+        </form>
+
+          <button phx-click="refresh">
+            <img src="images/refresh.svg">
+            Refresh
+          </button>
+      </div>
     </div>
     """
+  end
+
+  defp refresh_options do
+    [{"1s", 1}, {"5s", 5}, {"15s", 15}, {"30s", 30}, {"60s", 60}]
   end
 
   def handle_event("refresh", _params, socket) do
@@ -56,17 +72,27 @@ defmodule LiveViewStudioWeb.SalesDashboardLive do
     {:noreply, socket}
   end
 
+  def handle_event("select-refresh", %{"refresh" => refresh}, socket) do
+    refresh = String.to_integer(refresh)
+    socket = assign(socket, refresh: refresh)
+    {:noreply, socket}
+  end
+
   def handle_info(:tick, socket) do
     socket = assign_stats(socket)
+    schedule_refresh(socket)
     {:noreply, socket}
+  end
+
+  defp schedule_refresh(socket) do
+    Process.send_after(self(), :tick, socket.assigns.refresh * 1000)
   end
 
   defp assign_stats(socket) do
     assign(socket,
-        sales_amount: Sales.generate_sales(),
-        new_orders: Sales.generate_orders(),
-        satisfaction: Sales.generate_satisfaction()
+      sales_amount: Sales.generate_sales(),
+      new_orders: Sales.generate_orders(),
+      satisfaction: Sales.generate_satisfaction()
     )
   end
-
 end
